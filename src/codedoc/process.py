5a0ff613b09abbb0b3081f808a5aad94dfdfb1fc
mrenner-ML <manuelrenner@yahoo.fr>
2024-01-25 18:55:14 +0100

    feat: export docu to .md file

--- src/codedoc/process.py
+++ src/codedoc/process.py
@@ -3,34 +3,39 @@ import logging
 from pydantic import BaseModel, PrivateAttr
 
 from codeas.codebase import Codebase
-from codedoc.prompts import EXPLAIN_MODULE, SUMMARIZE_MODULE
 from codedoc.llm import Llm
-from codedoc.utils import read_file
+from codedoc.prompts import modules_desc_args, project_desc_args
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
-NUM_TOKENS = {
-    "short_desc": 40,
-    "long_desc": 200,
-}
 
 class RepoProcessor(BaseModel):
-    llm: Llm = Llm()
+    llm: Llm = Llm(model="gpt-3.5-turbo-1106")
     codebase: Codebase = Codebase()
-    _repo_map: dict = PrivateAttr({})
+    _docu: dict = PrivateAttr({})
     
     def generate_documentation(self):
         asyncio.run(self._generate_documentation())
+        self.write_markdown()
+    
+    def write_markdown(self):
+        md = "# Project Overview\n"
+        md += f"{self._docu['project_desc']}\n\n"
+        
+        md += "## Folder structure\n"
+        md += f"{self.codebase.get_tree()}\n\n"
+        
+        md += "## Modules\n"
+        for name, desc in self._docu["modules_desc"].items():
+            md += f"### {name}\n"
+            md += f"{desc}\n\n"
+
+        with open("modules-instruct.md", "w") as f:
+            f.write(md)
         
     async def _generate_documentation(self):
-        self._repo_map["modules_long_desc"] = await self.run_completions(
-            prompts_map=self.prompts_modules_long_desc(),
-            max_tokens= int(NUM_TOKENS["long_desc"]*1.2)
-        )
-        self._repo_map["modules_short_desc"] = await self.run_completions(
-            prompts_map=self.prompts_modules_short_desc(),
-            max_tokens= int(NUM_TOKENS["short_desc"]*1.2)
-        )
+        self._docu[f"modules_desc"] = await self.run_completions(**modules_desc_args(self))
+        self._docu[f"project_desc"] = await self.run_completions(**project_desc_args(self))
         
     async def run_completions(self, prompts_map: dict, **kwargs):
         prompts = list(prompts_map.keys())
@@ -38,40 +43,26 @@ class RepoProcessor(BaseModel):
         async for response, prompt in self.llm.run_batch_completions(
             prompts, **kwargs
         ):
-            logging.info("Gathering request")
+            # logging.info("Gathering request")
             if len(response.choices) == 1:
-                if isinstance(prompt, list):
+                if hasattr(response.choices[0], "message"):
                     key = prompts_map[prompt[0]["content"]]
                     responses[key] = response.choices[0].message.content
                 else:
-                    key = prompts_map[prompt]
+                    key = (
+                        prompts_map[prompt] if isinstance(prompt, str) 
+                        else prompts_map[prompt[0]]
+                    )
                     responses[key] = response.choices[0].text
             elif len(response.choices) > 1:
                 for choice in response.choices:
                     key = prompts_map[prompt[choice.index]]
                     responses[key] = choice.text
         return responses
-    
-    def prompts_modules_long_desc(self):
-        return {
-            EXPLAIN_MODULE.format(
-                code=read_file(path), num_tokens=NUM_TOKENS["long_desc"]
-            ): path 
-            for path in self.codebase.get_modules_paths()
-        }
-    
-    def prompts_modules_short_desc(self):
-        return {
-            SUMMARIZE_MODULE.format(
-                text=self._repo_map["modules_long_desc"][path], 
-                num_tokens=NUM_TOKENS["short_desc"]
-            ): path
-            for path in self.codebase.get_modules_paths()
-        }
         
 if __name__ == "__main__":
     processor = RepoProcessor(
         codebase={"base_dir": "./src"}, 
-        llm={"model": "gpt-3.5-turbo-instruct"})
+        llm={"model": "gpt-3.5-turbo-1106"})
     processor.generate_documentation()
-    processor._repo_map
+    processor._docu
--- src/codedoc/prompts.py
+++ src/codedoc/prompts.py
@@ -1,18 +1,55 @@
-EXPLAIN = """
-Explain in MAXIMUM {num_tokens} TOKENS what the following code does: 
-{code}
+from __future__ import annotations
+from codedoc.utils import read_file
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from codedoc.process import RepoProcessor
+
+NUM_TOKENS = {
+    "modules_desc": 50,
+    "project_desc": 50,
+}
+
+MODULE_DESC = """
+Write a short documentation explaining what the module does:
+{code}.
+The paragraph should be at most {num_tokens} tokens long.
 """.strip()
 
-EXPLAIN_MODULE = """
-Explain in around {num_tokens} tokens what the following module does. 
-DO NOT include the name of functions or classes in your explanation, only what is does.
-Be as concised as you can, trying to capture all of the functionalities in as few words as you can.
-CODE:
-{code}
+def modules_desc_args(processor: RepoProcessor):
+    return {
+        "prompts_map": {
+            MODULE_DESC.format(
+                code=read_file(path), num_tokens=NUM_TOKENS["modules_desc"]
+            ): path
+            for path in processor.codebase.get_modules_paths()
+        },
+        "max_tokens": int(NUM_TOKENS["modules_desc"]*1.2)
+    }
+    
+PROJECT_OVERVIEW = """
+Write a high level overview of the project that encapsulates its use cases and core functionalities.
+Try not to go over {num_tokens} tokens.
+
+Project structure:
+{tree}
+
+Modules:
+{modules}
 """.strip()
 
-SUMMARIZE_MODULE = """
-Summarize the following text related to a module in MAXIMUM {num_tokens} TOKENS.
-Be as concised as you can, trying to capture all of the functionalities in as few words as you can.
-{text}
-""".strip()
+def project_desc_args(processor: RepoProcessor):
+    modules_desc = ""
+    for module_path, module_desc in processor._docu["modules_desc"].items():
+        modules_desc += f"\t- {module_path}:\n\t\t{module_desc}\n"
+    return {
+        "prompts_map":
+            {
+                PROJECT_OVERVIEW.format(
+                    modules=modules_desc, 
+                    tree=processor.codebase.get_tree(),
+                    num_tokens=NUM_TOKENS["project_desc"]
+                ): 0
+            },
+        "max_tokens": int(NUM_TOKENS["project_desc"]*1.2)
+    }
